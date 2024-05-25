c66c5a0f6fbe15542180175c5131188106668a39
madogiwa0124 <madogiwa0124@gmail.com>
2024-05-25 18:47:30 +0900

    add base style.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    './app/components/**/*.html.erb',
+    './app/components/**/*.rb',
     './app/helpers/**/*.rb',
     './app/frontend/**/*.js',
     './app/frontend/**/*.css',
   ],
-  prefix: 'tw-',
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        'body': {
+          fontFamily: 'system-ui, sans-serif',
+          fontSize: '1rem',
+          fontHeight: '1.5',
+        },
+        'h1': {
+          fontSize: theme('fontSize.3xl'),
+          marginBottom: theme('margin.4'),
+        },
+      })
+    })
+  ],
+  prefix: 'tw-'
 }
